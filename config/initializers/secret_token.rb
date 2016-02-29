913fbffeb04657ddf0f965d0d030d5e368891310
Janel Stedtler <jstedtler@gmail.com>
2016-02-29 03:13:56 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Janellias::Application.config.secret_key_base = '6b93992c12d5dc0a35cd23bdccd42bf170487e9cd7621317bad21b9a2240d39095b378badaf0b9762eee39ee572e4009d2dc1b08a275602e79b747c1899d9424'
